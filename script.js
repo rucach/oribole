3fed0343155e05cb8e55524d2e1b39e99f297288
Rucach <98107806+rucach@users.noreply.github.com>
2022-07-10 14:08:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function loadit(){
+    console.log("MAIN SCREEN ('INDEX.HTML') LOADED");
+    setTimeout(function()
+    {
+        window.alert("IF YOU LIKE THIS APP OPEN THIS 👇");
+            window.alert("");
+    }, 10000);
+}
+function loading(){
+    document.getElementsByTagName("img").style.color = red;
+    document.getElementsByTagName("img").style.margin = 5 ;
+}
